965fcb9b7fbe8d953d3799b4d1b47255aeb14c0a
PARK SUNG DONG <oraedoa@gmail.com>
2020-02-13 18:07:23 +0900

    [test] 로또 익셉션 테스트 작성

--- src/test/java/lotto/dto/WonTest.java
+++ src/test/java/lotto/dto/WonTest.java
@@ -24,14 +24,14 @@ class WonTest {
         assertTrue(e.getMessage().contains("로또 구입 금액은 널 또는 공백이 될 수 없습니다."));
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"-1", "-2", "-3"})
+    @ParameterizedTest(name = "{0}원 금액 입력")
+    @ValueSource(strings = {"0", "-1", "-2", "-3"})
     @DisplayName("음수가 들어올 수 없습니다.")
     void moneyConstructorNegativeCheckTest(final String money) {
 
         Exception e = assertThrows(IllegalArgumentException.class, () -> new Won(money));
 
-        assertTrue(e.getMessage().contains("로또 구입 금액은 음수가 될 수 없습니다."));
+        assertTrue(e.getMessage().contains("들어온 금액이 음수 또는 0이기 때문에 로또를 구입할 수 없습니다."));
     }
 
 
